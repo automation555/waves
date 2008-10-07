0d178acbb86b5761a97fafd66fa3c3560bbcaf1e
ab5tract <ethyrnet@yahoo.com>
2008-10-07 01:59:47 -0400

    Fixed samples/blog and moved forward with app generation

--- samples/blog/configurations/development.rb
+++ samples/blog/configurations/development.rb
@@ -5,26 +5,19 @@ module Blog
     class Development < Default
 
       database :adapter => 'sqlite', :database => 'blog.db'
-
       reloadable [ Blog ]
-
       log :level => :debug
-      
       session :duration => 45.minutes, :path => 'tmp/sessions'
-
       host '127.0.0.1'
-
       port 4000
-
-#      handler ::Rack::Handler::Mongrel, :Host => host, :Port => port
-      # handler ::Rack::Handler::WEBrick, :BindAddress => host, :Port => port
-      # handler ::Rack::Handler::Thin, :Host => host, :Port => port
 
       application do
         use ::Rack::ShowExceptions
         use ::Rack::Static, :urls => [ '/images/', '/css/', '/javascript/', '/favicon.ico' ], :root => 'public'
         run ::Waves::Dispatchers::Default.new
       end
+
+
 
     end
 
